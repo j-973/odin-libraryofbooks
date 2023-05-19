f3cbd76065d4233986a30645fcaf1d99a58f6322
j-973 <jvoorheis973@gmail.com>
2023-05-19 17:16:38 -0400

    added page header, a wrapper for each .card for each book. Created showBooks function to iterate through the library array of books, each iteration makes DOM elements for a library book and sets the text content to each book's properties with dot notation. Added basic css styling

--- script.js
+++ script.js
@@ -24,3 +24,41 @@ function addBookToLibrary() {
 }
 
 addBookToLibrary();
+
+
+let wrapper = document.getElementById('wrapper');
+
+//iterates throuh the library array. Each iteration makes a card, card text, and DOM elements for every property of a book.
+//Text of DOM elements set to book properties with dot notation 
+showBooks = () => {
+  for (let i = 0; i < library.length; i++) {
+    let book = library[i];
+    let card = document.createElement('div'), 
+        cardText = document.createElement('div');
+    card.appendChild(cardText);
+
+    card.classList.add('card');
+    cardText.classList.add('card-text');
+
+    let title = document.createElement('h2'),
+      author = document.createElement('p'),
+      publishDate = document.createElement('p'),
+      genre = document.createElement('p');
+
+    title.textContent = book.title;
+    cardText.appendChild(title);
+
+    author.textContent = `Author: ${book.authorFirst} ${book.authorLast}`;
+    cardText.appendChild(author);
+
+    publishDate.textContent = `Publish Date: ${book.publishDate}`;
+    cardText.appendChild(publishDate);
+
+    genre.textContent = `Genre: ${book.genre}`;
+    cardText.appendChild(genre);
+
+    wrapper.appendChild(card);
+  }
+}
+
+showBooks();
