2ed60b542a81ade707d73b755d5f59998e3f9933
j-973 <jvoorheis973@gmail.com>
2023-05-23 18:06:09 -0400

    added Book properties for number of pages and read/unread status

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let library = [];
 
 //constructor function to define the properties of a book to be added to the library array.
-function Book(title, authorFirst, authorLast, genre, publishDate) {
+function Book(title, authorFirst, authorLast, genre, publishDate, numPages, readStatus) {
   this.title = title;
   this.authorFirst = authorFirst;
   this.authorLast = authorLast;
   this.genre = genre;
   this.publishDate = publishDate;
+  this.numPages = numPages;
+  this.readStatus = readStatus;
 }
 
 let wrapper = document.getElementById('wrapper');
@@ -22,7 +24,9 @@ createCard = (book) => {
   let title = document.createElement('h2'),
     author = document.createElement('p'),
     genre = document.createElement('p'),
-    publishDate = document.createElement('p');
+    publishDate = document.createElement('p'),
+    numPages = document.createElement('p'),
+    readStatus = document.createElement('p');
 
   title.textContent = book.title;
   cardText.appendChild(title);
@@ -35,6 +39,12 @@ createCard = (book) => {
 
   publishDate.textContent = `Publish Date: ${book.publishDate}`;
   cardText.appendChild(publishDate);
+
+  numPages.textContent = `Number of Pages: ${book.numPages}`;
+  cardText.appendChild(numPages);
+
+  readStatus.textContent = `Book Read?: ${book.readStatus}`;
+  cardText.appendChild(readStatus);
 
   return card;
 }
@@ -76,6 +86,18 @@ createBookForm = () => {
   publishDateInput.setAttribute('placeholder', 'Publish Date (mm/dd/yyyy)');
   bookForm.appendChild(publishDateInput);
 
+  let numPagesInput = document.createElement('input');
+  numPagesInput.setAttribute('type', 'number');
+  numPagesInput.setAttribute('placeholder', '# of Pages');
+  bookForm.appendChild(numPagesInput);
+
+  let readStatusInput = document.createElement('input');
+  let readStatusLabel = document.createElement('label');
+  readStatusInput.setAttribute('type', 'checkbox');
+  readStatusLabel.textContent = 'Book read?:'
+  bookForm.appendChild(readStatusLabel);
+  readStatusLabel.appendChild(readStatusInput);
+
   let submitButton = document.createElement('button');
   submitButton.textContent = 'Add New Book';
   bookForm.appendChild(submitButton);
@@ -96,9 +118,12 @@ addBookToLibrary = (ev) => {
       authorFirst = bookForm.querySelector('[placeholder="Author First Name"]').value,
       authorLast = bookForm.querySelector('[placeholder="Author Last Name"]').value,
       genre = bookForm.querySelector('[placeholder="Genre"]').value,
-      publishDate = bookForm.querySelector('[type="date"]').value;
+      publishDate = bookForm.querySelector('[type="date"]').value,
+      numPages = bookForm.querySelector('[placeholder="# of Pages"]').value,
+      readStatus = bookForm.querySelector('[type="checkbox"]');
+        readStatus = readStatus.checked;
 
-    let book = new Book(title, authorFirst, authorLast, genre, publishDate);
+    let book = new Book(title, authorFirst, authorLast, genre, publishDate, numPages, readStatus);
     library.push(book);
 
     bookForm.remove();
