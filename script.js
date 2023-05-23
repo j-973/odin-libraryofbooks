a815da29ed390f694459223fecb065004e9bd94c
j-973 <jvoorheis973@gmail.com>
2023-05-22 22:47:56 -0400

    added New Book button for adding a single book card to the library. Renamed showBooks function to createCardsFromLibrary. Moved card creation to its own function createCard, which returns a card from a book object it takes as its argument. Can be called when adding a single book to library, or when creating cards from the many books already in library.

--- script.js
+++ script.js
@@ -1,64 +1,69 @@
 let library = [];
 
 //constructor function to define the properties of a book to be added to the library array.
-function Book(title, authorFirst, authorLast, publishDate, genre) {
+function Book(title, authorFirst, authorLast, genre, publishDate) {
   this.title = title;
   this.authorFirst = authorFirst;
   this.authorLast = authorLast;
-  this.publishDate = publishDate;
   this.genre = genre;
+  this.publishDate = publishDate;
 }
 
-//after prompting the user for input information on the properties of a book, an instance of the book object is created with the 'new' keyword, and added to the library array with push().
-function addBookToLibrary() {
-    let title = prompt("What's the title of the book you want to add?:");
-    let authorFirst = prompt("What's the book author's first name?:");
-    let authorLast = prompt("What's the author's last name?:");
-    let publishDate = prompt("When was the book published? Enter as mm/dd/yyyy:");
-    let genre = prompt("What's the book's genre?:");
-  
-    let book = new Book(title, authorFirst, authorLast, publishDate, genre);
-    library.push(book);
+//moved book card creation to its own function which returns a card.
+//Can be called when adding a book to the library, or when creating cards from the existing books in the library array
+createCard = (book) => {
+  let card = document.createElement('div'),
+    cardText = document.createElement('div');
+  card.appendChild(cardText);
 
-    console.log(library);
+  card.classList.add('card');
+  cardText.classList.add('card-text');
+
+  let title = document.createElement('h2'),
+    author = document.createElement('p'),
+    genre = document.createElement('p'),
+    publishDate = document.createElement('p');
+
+  title.textContent = book.title;
+  cardText.appendChild(title);
+
+  author.textContent = `Author: ${book.authorFirst} ${book.authorLast}`;
+  cardText.appendChild(author);
+
+  genre.textContent = `Genre: ${book.genre}`;
+  cardText.appendChild(genre);
+
+  publishDate.textContent = `Publish Date: ${book.publishDate}`;
+  cardText.appendChild(publishDate);
+
+  return card;
 }
 
-addBookToLibrary();
-
-
-let wrapper = document.getElementById('wrapper');
-
-//iterates throuh the library array. Each iteration makes a card, card text, and DOM elements for every property of a book.
-//Text of DOM elements set to book properties with dot notation 
-showBooks = () => {
+createCardsFromLibrary = () => {
   for (let i = 0; i < library.length; i++) {
     let book = library[i];
-    let card = document.createElement('div'), 
-        cardText = document.createElement('div');
-    card.appendChild(cardText);
-
-    card.classList.add('card');
-    cardText.classList.add('card-text');
-
-    let title = document.createElement('h2'),
-      author = document.createElement('p'),
-      publishDate = document.createElement('p'),
-      genre = document.createElement('p');
-
-    title.textContent = book.title;
-    cardText.appendChild(title);
-
-    author.textContent = `Author: ${book.authorFirst} ${book.authorLast}`;
-    cardText.appendChild(author);
-
-    publishDate.textContent = `Publish Date: ${book.publishDate}`;
-    cardText.appendChild(publishDate);
-
-    genre.textContent = `Genre: ${book.genre}`;
-    cardText.appendChild(genre);
-
+    let card = createCard(book);
     wrapper.appendChild(card);
   }
 }
 
-showBooks();
+//after prompting the user for input information on the properties of a book, an instance of the book object is created with the 'new' keyword, and added to the library array with push().
+addBookToLibrary = () => {
+    let title = prompt("What's the title of the book you want to add?:");
+    let authorFirst = prompt("What's the book author's first name?:");
+    let authorLast = prompt("What's the author's last name?:");
+    let genre = prompt("What's the book's genre?:");
+    let publishDate = prompt("When was the book published? Enter as mm/dd/yyyy:");
+  
+    let book = new Book(title, authorFirst, authorLast, genre, publishDate);
+    library.push(book);
+
+    let card = createCard(book);
+    wrapper.appendChild(card);
+}
+
+
+
+let wrapper = document.getElementById('wrapper');
+
+document.addEventListener("click", addBookToLibrary);
